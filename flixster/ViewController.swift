3638ad3a0f5792046f137844729748f3689e4470
Kelly von Borstel <kellyvonborstel@gmail.com>
2019-10-15 01:32:15 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Kelly von Borstel on 10/15/19.
+//  Copyright © 2019 kellyvonborstel@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
