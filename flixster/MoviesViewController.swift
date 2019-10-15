435e2d0e320885c354a285dfa12ed63aef6af238
Kelly von Borstel <kellyvonborstel@gmail.com>
2019-10-15 02:39:34 -0700

    Add assets and create launch screen

---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Kelly von Borstel on 10/15/19.
+//  Copyright © 2019 kellyvonborstel@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
