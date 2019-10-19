ae2d9d410f62598f8c95829a4e8363d0b66cccba
Kelly von Borstel <kellyvonborstel@gmail.com>
2019-10-19 00:44:49 -0700

    Set up collection view for superhero movie posters

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Kelly von Borstel on 10/19/19.
+//  Copyright © 2019 kellyvonborstel@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
